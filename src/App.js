b37851f40197124083dee7b5f60610c51d1cb4b5
joseF <jose.farelo@hotmail.com>
2024-06-06 12:24:36 -0300

    Solución de Errores:     - Recarga constante al iniciar sesión (Se generaba un bucle en el useState de App.js al tardar en reconocer el token).

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainOut from './components/MainOut';
 import ConoceMedicar from "./components/ConoceMedicar";
@@ -13,18 +13,7 @@ import PerfilProfesional from "./components/PerfilProfesional";
 import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
-  
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    const userType = localStorage.getItem('userType');
-    if (token) {
-      const redirectUrl = userType === 'nRadio' ? "/inicio" : "/inicio-profesional";
-      window.location.href = redirectUrl;
-    }
-  }, []);
-
   return (
-
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainOut />} />
@@ -34,16 +23,16 @@ function App() {
         <Route path="/codigo-de-conducta" element={<CodigoConducta />} />
         <Route path="/iniciar-sesion" element={<IniciarSesion />} />
         <Route path="/registrarse" element={<Registrarse />} />
-        <Route element={<ProtectedRoute />}>
+        <Route element={<ProtectedRoute userType="nRadio" />}>
           <Route path="/inicio" element={<InicioUsuario />} />
+        </Route>
+        <Route element={<ProtectedRoute userType="pRadio" />}>
           <Route path="/inicio-profesional" element={<InicioProfesional />} />
           <Route path="/perfil-profesional" element={<PerfilProfesional />} />
         </Route>
       </Routes>
     </BrowserRouter>
-
   );
 }
 
 export default App;
-
--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
-const ProtectedRoute = ({ userType, ...rest }) => {
+const ProtectedRoute = ({ userType }) => {
     const token = localStorage.getItem('token');
     const storedUserType = localStorage.getItem('userType');
 
+    console.log("Token:", token);
+    console.log("Stored UserType:", storedUserType);
+    console.log("Required UserType:", userType);
+
     if (!token) {
+        console.log("No token, redirecting to /iniciar-sesion");
         return <Navigate to="/iniciar-sesion" />;
     }
 
     if (userType && userType !== storedUserType) {
+        console.log("UserType mismatch, redirecting to /iniciar-sesion");
         return <Navigate to="/iniciar-sesion" />;
     }
 
